77251a9841ba05302bd4fad30f85df1f8910b191
saumya <saumya@pivotaldesign.biz>
2016-05-23 12:42:22 +0530

    init

---
+++ MyContacts/VCRoot.swift
@@ -0,0 +1,22 @@
+//
+//  VCRoot.swift
+//  MyContacts
+//
+//  Created by Pivotal on 5/23/16.
+//  Copyright © 2016 pivotaldesign.biz. All rights reserved.
+//
+
+import UIKit
+
+class VCRoot : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //
+        
+    }
+}
